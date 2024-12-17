b19f8b36b45a499fe2dbfcf53fca0c08bb90e473
ducduyn31 <ducduy.ababat@gmail.com>
2024-12-17 18:30:19 +1100

    fix: build app before deploy

--- apps/dashboard/sst.config.ts
+++ apps/dashboard/sst.config.ts
@@ -21,8 +21,6 @@ export default $config({
           process.env.DEPLOY_ENV === "production"
             ? "coffeewithegg.com"
             : `${process.env.DEPLOY_ENV}.coffeewithegg.com`,
-        dns: false,
-        cert: "arn:aws:acm:us-east-1:031252989960:certificate/3f6cd3f6-1dac-4f19-9ae4-d78cd1cbf0cb",
         redirects: ["www.coffeewithegg.com"],
       },
     });
