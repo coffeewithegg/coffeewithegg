5ee68aa81a9f6dbd6ce350dd6e2d7e861f0d6b8d
ducduyn31 <ducduy.ababat@gmail.com>
2024-12-18 22:21:13 +1100

    feat(Ops): init platform (#5)
    * feat(Ops): init platform
    
    * fix: mismatch types
    
    * feat(Auth): add authentication

---
+++ apps/ops/app/routes/home.tsx
@@ -0,0 +1,21 @@
+import type { Route } from "./+types/home";
+
+export function meta({}: Route.MetaArgs) {
+  return [
+    { title: "CWE | Ops" },
+    { name: "description", content: "Managing site for CoffeeWithEgg" },
+  ];
+}
+
+export function loader({ context }: Route.LoaderArgs) {
+  return { message: context.VALUE_FROM_VERCEL };
+}
+
+export default function Home({ loaderData }: Route.ComponentProps) {
+  return (
+    <div>
+      <h1>Welcome to React Router!</h1>
+      <p>{loaderData.message}</p>
+    </div>
+  );
+}
